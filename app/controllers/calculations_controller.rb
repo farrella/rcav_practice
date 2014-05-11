c70bbab23658d3b0ac111af9612c1d2a70e21087
Abby Farrell <abbykass@gmail.com>
2014-05-10 20:06:08 -0500

    Little Calc Finished

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -12,15 +12,11 @@ class CalculationsController < ApplicationController
     @answer = @the_number*@the_number.to_f
   end
 
-
-
   def pmt
-   	@rate = params[:interest_rate].to_f
+   	@rate = params[:interest_rate].to_f / 12 / 100
    	@nper = params[:number_of_payments].to_f
    	@pv = params[:present_value].to_f
 	@payment = (@rate*@pv).to_f/(1-(1+@rate)**-@nper).to_f
-
-
 
   end
 end
