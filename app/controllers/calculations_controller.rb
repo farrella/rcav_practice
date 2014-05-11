e6fece44126459d4d3807c18f7a0e9b470b7046a
Abby Farrell <abbykass@gmail.com>
2014-05-10 19:58:57 -0500

    Little Calc - payment done but w/out formatting

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -4,15 +4,24 @@ class CalculationsController < ApplicationController
 
   def sqrt
     @the_number = params[:number].to_f
-    @answer = Math.sqrt(@the_number)
+    @answer = Math.sqrt(@the_number).to_f
   end
 
   def square
   	@the_number = params[:number].to_f
-    @answer = @the_number*@the_number
+    @answer = @the_number*@the_number.to_f
   end
 
+
+
   def pmt
+   	@rate = params[:interest_rate].to_f
+   	@nper = params[:number_of_payments].to_f
+   	@pv = params[:present_value].to_f
+	@payment = (@rate*@pv).to_f/(1-(1+@rate)**-@nper).to_f
+
+
+
   end
 end
 
